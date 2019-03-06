29f96ee1f7d38640c75a06219e5c62a95f9315af
lingxiaopua <1083514716@qq.com>
2019-03-06 18:11:20 +0800

    编写登录界面

--- app/src/main/java/com/bilibili/lingxiao/MainActivity.kt
+++ app/src/main/java/com/bilibili/lingxiao/MainActivity.kt
@@ -5,8 +5,10 @@ import android.os.Bundle
 import android.support.v7.app.ActionBarDrawerToggle
 import android.view.MenuItem
 import android.view.View
+import com.bilibili.lingxiao.user.LoginActivity
 import com.camera.lingxiao.common.app.BaseActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.nav_header.*
 
 class MainActivity : BaseActivity() {
     var tabArray = arrayOf("直播","推荐","追番","分区","动态","发现")
@@ -15,10 +17,10 @@ class MainActivity : BaseActivity() {
 
     override fun initWidget() {
         super.initWidget()
-        setSupportActionBar(main_toolbar);
+        setSupportActionBar(main_toolbar)
         //设置返回键可用
-        getSupportActionBar()?.setHomeButtonEnabled(true);
-        getSupportActionBar()?.setDisplayHomeAsUpEnabled(true);
+        supportActionBar?.setHomeButtonEnabled(true);
+        supportActionBar?.setDisplayHomeAsUpEnabled(true);
         var drawerToggle = object : ActionBarDrawerToggle(this,main_drawer_layout,main_toolbar,R.string.open,R.string.close){
             override fun onDrawerOpened(drawerView: View) {
                 super.onDrawerOpened(drawerView)
@@ -33,9 +35,10 @@ class MainActivity : BaseActivity() {
         for (name in tabArray){
             main_tabLayout.addTab(main_tabLayout.newTab().setText(name))
         }
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        return super.onOptionsItemSelected(item)
+        var navigationView = main_navigation.inflateHeaderView(R.layout.nav_header)
+        var header_view = navigationView.findViewById<View>(R.id.nav_header)
+        header_view.setOnClickListener({
+            StartActivity(LoginActivity::class.java,false)
+        })
     }
 }
--- app/src/main/java/com/bilibili/lingxiao/user/LoginActivity.kt
+++ app/src/main/java/com/bilibili/lingxiao/user/LoginActivity.kt
@@ -2,8 +2,10 @@ package com.bilibili.lingxiao.user
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.bilibili.lingxiao.R
 import com.camera.lingxiao.common.app.BaseActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : BaseActivity() {
     override val contentLayoutId: Int
@@ -11,6 +13,23 @@ class LoginActivity : BaseActivity() {
 
     override fun initWidget() {
         super.initWidget()
+        setToolbarBack(login_toolbar)
+        login_toolbar.title = "登录"
+        login_et_password.setOnFocusChangeListener(object : View.OnFocusChangeListener{
+            override fun onFocusChange(v: View?, hasFocus: Boolean) {
+                if (hasFocus){
+                    login_img_22.setImageResource(R.drawable.ic_22_hide)
+                    login_img_33.setImageResource(R.drawable.ic_33_hide)
+                    login_line_between_password.setBackgroundColor(resources.getColor(R.color.colorPrimary))
+                    login_line_between_username.setBackgroundColor(resources.getColor(R.color.black_alpha_16))
+                }else{
+                    login_img_22.setImageResource(R.drawable.ic_22)
+                    login_img_33.setImageResource(R.drawable.ic_33)
+                    login_line_between_username.setBackgroundColor(resources.getColor(R.color.colorPrimary))
+                    login_line_between_password.setBackgroundColor(resources.getColor(R.color.black_alpha_16))
+                }
+            }
+        })
     }
 
 }
