5c9aa15b49752a21eae3d86e5579499ee4ba867b
lingxiaopua <1083514716@qq.com>
2019-04-02 18:24:44 +0800

    将endpage显示出来

--- app/src/main/java/com/bilibili/lingxiao/play/IntroduceFragment.kt
+++ app/src/main/java/com/bilibili/lingxiao/play/IntroduceFragment.kt
@@ -1,11 +1,15 @@
 package com.bilibili.lingxiao.play
 
+import android.graphics.drawable.Drawable
+import android.support.v7.widget.GridLayoutManager
 import android.view.View
 import com.bilibili.lingxiao.R
 import com.bilibili.lingxiao.dagger.DaggerUiComponent
 import com.camera.lingxiao.common.app.BaseFragment
+import kotlinx.android.synthetic.main.fragment_introduce.view.*
 
 class IntroduceFragment :BaseFragment(){
+    var mEndPageList = arrayListOf<EndPageData>()
     override val contentLayoutId: Int
         get() = R.layout.fragment_introduce
 
@@ -15,6 +19,26 @@ class IntroduceFragment :BaseFragment(){
     }
     override fun initWidget(root: View) {
         super.initWidget(root)
+        var manager = GridLayoutManager(context,5)
+        root.endpage_recycler.layoutManager = manager
+        setEndPageData()
+        root.endpage_recycler.adapter = EndPageAdapter(R.layout.item_endpage,mEndPageList)
+        root.endpage_recycler.isNestedScrollingEnabled = false
+    }
+
+    fun setEndPageData(){
+        var recommend = EndPageData(resources.getDrawable(R.drawable.ic_recommend),"123")
+        var dislike = EndPageData(resources.getDrawable(R.drawable.ic_dislike),"不喜欢")
+        var coin = EndPageData(resources.getDrawable(R.drawable.ic_coin),"1")
+        var collect = EndPageData(resources.getDrawable(R.drawable.ic_collect),"12")
+        var share = EndPageData(resources.getDrawable(R.drawable.ic_share),"12")
+        mEndPageList.add(recommend)
+        mEndPageList.add(dislike)
+        mEndPageList.add(coin)
+        mEndPageList.add(collect)
+        mEndPageList.add(share)
+    }
+    data class EndPageData(val icon:Drawable,val message:String){
 
     }
 }
